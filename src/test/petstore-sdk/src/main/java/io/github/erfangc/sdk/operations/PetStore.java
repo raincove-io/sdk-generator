2b7dca83e0ee4062811d0a0ad184ca38326e0b3b
Erfang Chen <erfang.chen@blackrock.com>
2019-06-27 14:16:21 -0400

    adding content type

--- src/test/petstore-sdk/src/main/java/io/github/erfangc/sdk/operations/PetStore.java
+++ src/test/petstore-sdk/src/main/java/io/github/erfangc/sdk/operations/PetStore.java
@@ -8,12 +8,15 @@ import io.github.erfangc.sdk.models.Pets;
 public interface PetStore {
 
     @RequestLine("GET /pets?limit={limit}")
+    @Headers({"Content-Type: application/json", "Accept: application/json"})
     Pets listPets(@Param("limit") Integer limit);
 
     @RequestLine("POST /pets?nosave={nosave}&test={test}")
+    @Headers({"Content-Type: application/json", "Accept: application/json"})
     void createPets(Pet body, @Param("nosave") String nosave, @Param("test") Boolean test);
 
     @RequestLine("GET /pets/{petId}")
+    @Headers({"Content-Type: application/json", "Accept: application/json"})
     Pets showPetById(@Param("petId") String petId);
 
 }
